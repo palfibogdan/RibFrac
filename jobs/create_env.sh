f7c0640a39e33951308b2f37ff7b585bfd6f9f62
Alexandru Turcu <alexandru.turcu2000@gmail.com>
2023-09-25 15:45:49 +0200

    added create_env.sh

---
+++ jobs/create_env.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=01:00:00
+ 
+# Execute program located in $HOME
+module load 2022
+module load Anaconda3/2022.05
+
+git clone https://github.com/Project-MONAI/MONAI.git
+cd MONAI/
+conda create -n medical python=3.8
+conda env update -n medical -f environment-dev.yml
