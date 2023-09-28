4531c6618a4c04f0f75699655e12db7f828238f2
Alexandru Turcu <scur0625@int6.local.snellius.surf.nl>
2023-09-28 14:27:26 +0200

    added script for training unet

---
+++ jobs/train_nnunet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=30:00:00
+#SBATCH --output=train_nnunet.out
+#SBATCH --job-name=train_nnunet
+
+# Execute program located in $HOME
+conda activate medical
+
+cd RibFrac
+srun python Code/nn-unet.py --mode train --train_folder "data/train" --val_folder "data/val"
